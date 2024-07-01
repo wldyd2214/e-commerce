34a7cad125e7f67fe220d6820ac8bc94aa408d40
wldyd2214 <39686405+wldyd2214@users.noreply.github.com>
2024-07-01 20:45:39 +0900

    Initial commit

---
+++ src/test/java/com/hhplus/commerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
