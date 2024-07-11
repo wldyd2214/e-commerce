e3de92293d6bb66c3d3b8f4fd5b801dd54dea16a
wldyd2214 <39686405+wldyd2214@users.noreply.github.com>
2024-07-12 07:41:26 +0900

    Docs: 상위 상품 목록 조회에 대한 상세 설명 주석 추가

--- src/main/java/com/hhplus/commerce/spring/infrastructure/db/OrderItemRepositoryImpl.java
+++ src/main/java/com/hhplus/commerce/spring/infrastructure/db/OrderItemRepositoryImpl.java
@@ -30,18 +30,29 @@ public class OrderItemRepositoryImpl implements OrderItemRepository {
 
         LocalDate threeDaysAgo = LocalDate.now().minusDays(3);
 
-        // 쿼리
-//        SELECT product_id, SUM(order_product_count) as order_product_count
-//        FROM order_item
-//        WHERE reg_date >= CURDATE() - INTERVAL 3 DAY
-//        GROUP BY product_id
-//        ORDER BY order_product_count DESC
-//        LIMIT 5;
+        // TODO: 쿼리
+        /*
+           SELECT product_id, SUM(order_product_count) as order_product_count
+           FROM order_item
+           WHERE reg_date >= CURDATE() - INTERVAL 3 DAY
+           GROUP BY product_id
+           ORDER BY order_product_count DESC
+           LIMIT 5;
+         */
 
-        // 인덱스 추가
-//        CREATE INDEX idx_order_date ON order_item(reg_date);
-//        CREATE INDEX idx_product_id ON order_item(product_id);
-//        CREATE INDEX idx_order_date_product_id ON order_item(reg_date, product_id);
+
+        // TODO: 인덱스 추가
+        /*
+
+        // 쿼리에서 최근 3일간의 데이터를 필터링하는데 사용되어 조회 속도를 향상시키기 위해 인덱스 추가
+        CREATE INDEX idx_order_date ON order_item(reg_date);
+
+        // 쿼리에서 그룹과 집계에 사용되어 조회 속도를 향상시키기 위해 인덱스 추가
+        CREATE INDEX idx_product_id ON order_item(product_id);
+
+        // reg_date, product_id 컬럼에 대한 복한 인덱스를 추가하여 속도를 향상
+        CREATE INDEX idx_order_date_product_id ON order_item(reg_date, product_id);
+        */
 
         List<Tuple> tuples =
                 jpaQueryFactory
