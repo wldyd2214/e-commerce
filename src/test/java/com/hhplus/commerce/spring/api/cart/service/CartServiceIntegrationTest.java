ec773f005cefacc079a74e8b44a2776681a62d4a
wldyd2214 <wldyd2214@naver.com>
2024-07-19 01:24:41 +0900

    Test: 장바구니 테스트 코드 불필요 import 제거

--- src/test/java/com/hhplus/commerce/spring/api/cart/service/CartServiceIntegrationTest.java
+++ src/test/java/com/hhplus/commerce/spring/api/cart/service/CartServiceIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.hhplus.commerce.spring.api.cart.service;
 
-import static com.hhplus.commerce.spring.api.order.model.type.OrderStatus.COMPLETED;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.hhplus.commerce.spring.api.cart.infrastructure.database.CartItemJpaRepository;
@@ -24,9 +23,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.groups.Tuple.tuple;
-import static org.mockito.ArgumentMatchers.*;
 
 @SpringBootTest
 public class CartServiceIntegrationTest {
