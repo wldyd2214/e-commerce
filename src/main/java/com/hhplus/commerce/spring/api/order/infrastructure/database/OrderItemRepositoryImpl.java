0db85b8280f7f0c920b1e993d88b0b40d953160d
wldyd2214 <wldyd2214@naver.com>
2024-07-29 21:34:14 +0900

    Feat: 상위 상품 통계 Query 주문 상태 완료 조건 추가

--- src/main/java/com/hhplus/commerce/spring/api/order/infrastructure/database/OrderItemRepositoryImpl.java
+++ src/main/java/com/hhplus/commerce/spring/api/order/infrastructure/database/OrderItemRepositoryImpl.java
@@ -1,8 +1,7 @@
 package com.hhplus.commerce.spring.api.order.infrastructure.database;
 
-import static com.hhplus.commerce.spring.api.order.model.QOrderItem.orderItem;
-
 import com.hhplus.commerce.spring.api.order.model.OrderItem;
+import com.hhplus.commerce.spring.api.order.model.type.OrderStatus;
 import com.hhplus.commerce.spring.api.order.repository.OrderItemRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +13,9 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import java.time.LocalDate;
 import java.util.stream.Collectors;
+
+import static com.hhplus.commerce.spring.api.order.model.QOrderItem.orderItem;
+import static com.hhplus.commerce.spring.api.order.model.QOrder.order;
 
 @RequiredArgsConstructor
 @Repository
@@ -29,14 +31,13 @@ public class OrderItemRepositoryImpl implements OrderItemRepository {
     @Override
     public List<Product> selectPopularOrderItems() {
 
-        LocalDate threeDaysAgo = LocalDate.now().minusDays(3);
-
         // TODO: 쿼리
         /*
-           SELECT product_id, SUM(order_product_count) as order_product_count
-           FROM order_item
-           WHERE reg_date >= CURDATE() - INTERVAL 3 DAY
-           GROUP BY product_id
+           SELECT oi.product_id, SUM(oi.order_product_count) as order_product_count
+           FROM tb_order_item oi
+           JOIN tb_order o ON oi.order_id = o.order_id
+           WHERE o.order_status = "COMPLETED" AND oi.reg_date >= CURDATE() - INTERVAL 3 DAY
+           GROUP BY oi.product_id
            ORDER BY order_product_count DESC
            LIMIT 5;
          */
@@ -55,11 +56,14 @@ public class OrderItemRepositoryImpl implements OrderItemRepository {
         CREATE INDEX idx_order_date_product_id ON order_item(reg_date, product_id);
         */
 
+        LocalDate threeDaysAgo = LocalDate.now().minusDays(3);
+
         List<Tuple> tuples =
                 jpaQueryFactory
                         .select(orderItem.product, orderItem.orderProductCount.sum())
                         .from(orderItem)
-                        .where(orderItem.createdDateTime.after(threeDaysAgo.atStartOfDay()))
+                        .join(orderItem.order, order)
+                        .where(order.orderStatus.eq(OrderStatus.COMPLETED), orderItem.createdDateTime.after(threeDaysAgo.atStartOfDay()))
                         .groupBy(orderItem.product)
                         .orderBy(orderItem.orderProductCount.sum().desc())
                         .limit(5)
