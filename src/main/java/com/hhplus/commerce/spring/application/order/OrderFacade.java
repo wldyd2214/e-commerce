b43c8e00b5c95bad6617b13b4314047f1f5edb75
wldyd2214 <wldyd2214@naver.com>
2024-09-20 20:58:20 +0900

    Feat: 클린 아키텍처 적용 중

--- src/main/java/com/hhplus/commerce/spring/application/order/OrderFacade.java
+++ src/main/java/com/hhplus/commerce/spring/application/order/OrderFacade.java
@@ -2,18 +2,7 @@ package com.hhplus.commerce.spring.application.order;
 
 import com.hhplus.commerce.spring.application.order.dto.request.OrderFacadeRequest;
 import com.hhplus.commerce.spring.application.order.dto.response.OrderFacadeResponse;
-import com.hhplus.commerce.spring.application.order.mapper.OrderFacadeRequestMapper;
-import com.hhplus.commerce.spring.application.order.mapper.OrderFacadeResponseMapper;
-import com.hhplus.commerce.spring.domain.order.service.OrderService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
-@Service
-public class OrderFacade {
-    private final OrderService orderService;
-
-    public OrderFacadeResponse order(OrderFacadeRequest request) {
-        return OrderFacadeResponseMapper.toOrder(orderService.order(OrderFacadeRequestMapper.toOrder(request)));
-    }
+public interface OrderFacade {
+    OrderFacadeResponse order(OrderFacadeRequest request);
 }
