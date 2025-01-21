dc63feac0ea7bd511ae57140eb37bf3c876b6e10
wldyd2214 <wldyd2214@naver.com>
2025-01-21 23:54:24 +0900

    Chore: build.gradle querdsl 설정 변경

--- src/main/java/com/hhplus/commerce/spring/domain/payment/service/PaymentService.java
+++ src/main/java/com/hhplus/commerce/spring/domain/payment/service/PaymentService.java
@@ -1,7 +1,18 @@
 package com.hhplus.commerce.spring.domain.payment.service;
 
 import com.hhplus.commerce.spring.domain.payment.dto.PaymentCommand;
+import com.hhplus.commerce.spring.domain.payment.dto.response.PaymentSystemClientResponse;
+import com.hhplus.commerce.spring.infrastructure.payment.client.PaymentSystemClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public interface PaymentService {
-    String sendPayment(PaymentCommand.Payment command);
+@RequiredArgsConstructor
+@Service
+public class PaymentService {
+    private final PaymentSystemClient client;
+
+    public String sendPayment(PaymentCommand.Payment command) {
+        PaymentSystemClientResponse response = client.sendPayment(command.getUserId(), command.getAmount());
+        return response.getTransactionId();
+    }
 }
