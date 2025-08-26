ed7d036b2b823c84a9d1fc68e37aaac88fd5e682
Park Jiyong <39686405+wldyd2214@users.noreply.github.com>
2025-08-26 15:19:55 +0900

    test: UserService 사용자 포인트 충전 비즈니스 로직 테스트 추가

--- src/test/java/com/hhplus/commerce/spring/user/domain/service/UserServiceImplTest.java
+++ src/test/java/com/hhplus/commerce/spring/user/domain/service/UserServiceImplTest.java
@@ -1,4 +1,33 @@
-import static org.junit.jupiter.api.Assertions.*;
-class UserServiceImplTest {
-  
+package com.hhplus.commerce.spring.user.domain.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.hhplus.commerce.spring.user.domain.command.UserCommand;
+import com.hhplus.commerce.spring.user.domain.dto.UserSummaryInfo;
+import java.math.BigDecimal;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@Transactional
+public record UserServiceImplTest(UserService userService) {
+    
+    @Test
+    void chargeUserPoints() {
+        // given
+        Long userId = 1L;
+        BigDecimal chargePoint = BigDecimal.valueOf(100);
+        var command = UserCommand.ChargePoint.of(userId, chargePoint);
+
+        // when
+        UserSummaryInfo userInfo = userService.chargeUserPoints(command);
+
+        // that
+        assertThat(userInfo.getId()).isNotNull();
+        assertThat(userInfo.getPoint()).isEqualTo(command.getChargePoint());
+    }
+
 }
