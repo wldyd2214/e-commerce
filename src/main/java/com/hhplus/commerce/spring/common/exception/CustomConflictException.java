c6ddcc5dc0b79065cf7d57a75e6ce46e073ceb67
Park Jiyong <39686405+wldyd2214@users.noreply.github.com>
2025-08-27 10:20:17 +0900

    feat: conflict 예외 모듈 추가

--- src/main/java/com/hhplus/commerce/spring/common/exception/CustomConflictException.java
+++ src/main/java/com/hhplus/commerce/spring/common/exception/CustomConflictException.java
@@ -1,5 +1,21 @@
 package com.hhplus.commerce.spring.common.exception;
 
-public class CustomConflictExcpetion {
+import com.hhplus.commerce.spring.common.exception.code.ConflictErrorCode;
+import java.text.MessageFormat;
+import lombok.Getter;
 
+@Getter
+public class CustomConflictException extends CustomBaseException {
+
+    private final ConflictErrorCode errorCode;
+
+    public CustomConflictException(ConflictErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public CustomConflictException(ConflictErrorCode errorCode, String args) {
+        super(MessageFormat.format(errorCode.getMessage(), args));
+        this.errorCode = errorCode;
+    }
 }
--- src/main/java/com/hhplus/commerce/spring/common/exception/code/ConflictErrorCode.java
+++ src/main/java/com/hhplus/commerce/spring/common/exception/code/ConflictErrorCode.java
@@ -1,4 +1,16 @@
 package com.hhplus.commerce.spring.common.exception.code;
 
-public record ConflictErrorCode() {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ConflictErrorCode implements BaseErrorCode {
+    CONFLICT(HttpStatus.CONFLICT, "40900000", "이미 존재하는 리소스"),
+    DUPLICATE_EMAIL(HttpStatus.CONFLICT, "40900001", "이미 사용중인 이메일입니다: {0}");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
 }
