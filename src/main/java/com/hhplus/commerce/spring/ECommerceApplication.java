efb66540ab0665962e80154d00adca36a658d5f6
Park Jiyong <39686405+wldyd2214@users.noreply.github.com>
2025-08-28 11:49:59 +0900

    feat: EnableJpaAuditing 활성화 어노테이션 추가

--- src/main/java/com/hhplus/commerce/spring/ECommerceApplication.java
+++ src/main/java/com/hhplus/commerce/spring/ECommerceApplication.java
@@ -2,9 +2,10 @@ package com.hhplus.commerce.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@EnableCaching
 @SpringBootApplication
+@EnableJpaAuditing // JPA Auditing 활성화
 public class ECommerceApplication {
 
     public static void main(String[] args) {
