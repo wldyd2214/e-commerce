58b8e14eb8b1647625666e49992397921acf0ad9
Park Jiyong <39686405+wldyd2214@users.noreply.github.com>
2025-08-28 11:55:49 +0900

    docs: UserSummaryInfo 응답 컬럼 필수 여부 설정

--- src/main/java/com/hhplus/commerce/spring/user/domain/dto/UserSummaryInfo.java
+++ src/main/java/com/hhplus/commerce/spring/user/domain/dto/UserSummaryInfo.java
@@ -2,6 +2,7 @@ package com.hhplus.commerce.spring.user.domain.dto;
 
 import com.hhplus.commerce.spring.user.domain.entity.User;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -12,15 +13,19 @@ import lombok.Getter;
 public class UserSummaryInfo {
 
     @Schema(description = "유니크키", example = "101")
+    @NotNull
     private Long id;
 
     @Schema(description = "이메일", example = "jypark@gmail.com")
+    @NotNull
     private String email;
 
     @Schema(description = "이름", example = "박지용")
+    @NotNull
     private String name;
 
     @Schema(description = "잔여 포인트", example = "7000")
+    @NotNull
     private BigDecimal point;
 
     public static UserSummaryInfo of(User user) {
