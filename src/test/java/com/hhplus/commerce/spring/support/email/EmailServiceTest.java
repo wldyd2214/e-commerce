ade9b3177a254f0e30fb6a375c75caf556f47757
Park Jiyong <39686405+wldyd2214@users.noreply.github.com>
2025-09-01 14:07:00 +0900

    test: 이메일 전송 모듈 테스트 코드 작성

--- src/test/java/com/hhplus/commerce/spring/support/email/EmailServiceTest.java
+++ src/test/java/com/hhplus/commerce/spring/support/email/EmailServiceTest.java
@@ -1,4 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.hhplus.commerce.spring.support.email;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
 class EmailServiceTest {
-  
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void sendWelcomeEmail() {
+        // given
+        String toEmail = "test@example.com";
+        String userName = "Test User";
+
+        // when
+        boolean result = emailService.sendWelcomeEmail(toEmail, userName);
+
+        // that
+        assertThat(result).isTrue();
+    }
 }
