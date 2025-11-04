f1ee71525e06aad55cc5d1175b4348fd2adf01fd
wldyd2214 <wldyd2214@naver.com>
2025-11-04 21:38:34 +0900

    feat: 상품 목록 조회 요청 객체 레코드 변환

--- src/main/java/com/hhplus/commerce/spring/product/presentation/ProductController.java
+++ src/main/java/com/hhplus/commerce/spring/product/presentation/ProductController.java
@@ -1,13 +1,14 @@
 package com.hhplus.commerce.spring.product.presentation;
 
 import com.hhplus.commerce.spring.common.ApiResponse;
+import com.hhplus.commerce.spring.product.domain.dto.ProductSummaryInfo;
 import com.hhplus.commerce.spring.product.domain.service.ProductService;
 import com.hhplus.commerce.spring.product.presentation.request.GetProductsRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,15 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Product", description = "상품 관련 인터페이스를 제공합니다.")
 @RequestMapping(value = "/api/v1/products")
 public class ProductController {
-
     private final ProductService productService;
 
     @Operation(summary = "상품 목록 조회 API", description = "상품 목록을 조회합니다.")
     @GetMapping("")
-    public ApiResponse<Void> getProducts(@RequestParam GetProductsRequest request) {
-
+    public ApiResponse<Page<ProductSummaryInfo>> getProducts(@RequestParam GetProductsRequest request) {
 //        productService.getProducts();
-
         return ApiResponse.ok(null);
     }
 }
